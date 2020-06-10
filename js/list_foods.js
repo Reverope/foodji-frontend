963b0a02e520ed40efc65a554a53781e4bf79fc7
Reverope <shatish123456789@gmail.com>
2020-06-10 20:08:11 +0530

    SideNav Cart

--- js/list_foods.js
+++ js/list_foods.js
@@ -1,16 +1,18 @@
+// var loader = document.querySelector(".preloader");
 var container = document.querySelector("#ritem");
 var containerBig = document.getElementById("container");
+var orderAr = [];
 // var restaurantId = document.getElementById("restaurantId").innerText;
 function getParameterByName(name, url) {
   if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, '\\$&');
-  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-      results = regex.exec(url);
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
   if (!results) return null;
-  if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
-var restaurantId = getParameterByName('id')
+var restaurantId = getParameterByName("id");
 // console.log(restId)
 var url = "https://knight-foodji.herokuapp.com/api/restaurant/" + restaurantId;
 
@@ -37,14 +39,10 @@ fetch(url, {
       //   var img = food.childNodes[1].childNodes[1].childNodes[1].childNodes[1];
       //   img["attributes"][0]["value"] = element.image;
 
-      var name =
-        food.childNodes[1].childNodes[1].childNodes[3].childNodes[1]
-          .childNodes[1];
+      var name = food.childNodes[1].childNodes[3].childNodes[1].childNodes[1];
       name["innerText"] = foodItem["foodid"].name;
 
-      var price =
-        food.childNodes[1].childNodes[1].childNodes[3].childNodes[1]
-          .childNodes[3];
+      var price = food.childNodes[1].childNodes[3].childNodes[1].childNodes[3];
       price["innerText"] = foodItem.price;
 
       //   var address =
@@ -56,5 +54,68 @@ fetch(url, {
     });
   })
   .then((_) => {
+    // loader.remove();
     containerBig.removeChild(containerBig.childNodes[1]);
+  })
+  .then(() => {
+    const todoButton = document.querySelectorAll(".orderfoodbutton");
+    const deleteButton = document.querySelectorAll(".deleteorderfood");
+
+    [...todoButton].forEach((button) =>
+      button.addEventListener("click", (item) => {
+        //
+
+        const todoList = document.querySelector(".todo-list");
+        const todoListItem = document.querySelectorAll(".order-item");
+        todoListItem.forEach((_i) => {
+          _i.remove();
+        });
+        //
+        var foodItemClicked = item.composedPath()[1];
+        var foodItem = foodItemClicked["childNodes"][1];
+        var foodName = foodItem["childNodes"][1].innerText;
+        var foodPrice = foodItem["childNodes"][3].innerText;
+
+        orderAr.push({
+          food_name: foodName,
+          food_price: foodPrice,
+          quantity: 1,
+        });
+
+        console.log(orderAr.length);
+        orderAr.forEach((orderItem) => {
+          var foodItemInList = document.createElement("li");
+          foodItemInList.className = "order-item";
+
+          var foodItemPriceInlist = document.createElement("span");
+          foodItemPriceInlist.className = "pr";
+
+          foodItemInList.innerText = orderItem.food_name;
+
+          var priceandquan =
+            orderItem.food_price + " ( x" + orderItem.quantity + " )";
+
+          foodItemPriceInlist.innerText = priceandquan;
+
+          foodItemInList.appendChild(foodItemPriceInlist);
+
+          todoList.appendChild(foodItemInList);
+        });
+      })
+    );
+
+    [...deleteButton].forEach((button) => {
+      button.addEventListener("click", (item) => {
+        const todoList = document.querySelector(".todo-list");
+        const todoListItem = document.querySelectorAll(".order-item");
+        var foodItemClicked = item.composedPath()[2];
+
+        var foodItem = foodItemClicked["childNodes"];
+        var foodName = foodItem[1]["childNodes"][1].innerText;
+
+        // var found = orderAr.find((element) => element == foodName);
+      });
+    });
   });
+{
+}
