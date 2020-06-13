67d02b3d180ce06bfc57b2bbe912ee42e4fa121b
KaviiSuri <surikavii@gmail.com>
2020-06-13 12:02:51 +0530

    added delivery boy login

--- js/global.js
+++ js/global.js
@@ -23,11 +23,14 @@ function logOut() {
   localStorage.removeItem("foodji-user");
   localStorage.removeItem("foodji-rest-auth-header");
   localStorage.removeItem("foodji-rest");
+  localStorage.removeItem("foodji-guy-auth-header");
+  localStorage.removeItem("foodji-guy");
 }
 
 var loginModal = document.getElementById("login-modal");
 var signUpModal = document.getElementById("register-modal");
 var restLogInModal = document.getElementById("rest-modal");
+var guyLogInModal = document.getElementById("guy-modal");
 
 function logIn() {
   if (loginModal) loginModal.style.display = "block";
@@ -57,10 +60,17 @@ function logRest() {
   if (restLogInModal) restLogInModal.style.display = "block";
 }
 
+function logGuy() {
+  if (guyLogInModal) guyLogInModal.style.display = "block";
+}
+
 ready(function () {
   var logInOutBtn = document.getElementById("logInLogOutBtn");
   var signUpProfileBtn = document.getElementById("signUpProfileBtn");
   var token = localStorage.getItem("foodji-user-auth-header");
+
+  var restToken = localStorage.getItem("foodji-rest-auth-header");
+  var guyToken = localStorage.getItem("foodji-guy-auth-header");
 
   var loginHtml = '<a class="check" onclick="logIn()" href="#">Login</a>';
   var signUpHtml = '<a onclick="singUp()" href="#">Signup</a>';
@@ -68,7 +78,7 @@ ready(function () {
   var logoutHtml = '<a href="index.html" onclick="logOut()">LogOut</a>';
   var profileLinkHtml = '<a href="userprofile.html">Profile</a>';
 
-  if (!token) {
+  if (!token && !restToken && !guyToken) {
     console.log("Token does not exist");
     logInOutBtn.innerHTML = loginHtml;
     signUpProfileBtn.innerHTML = signUpHtml;
@@ -89,6 +99,10 @@ ready(function () {
   span3.onclick = function () {
     restLogInModal.style.diplay = "none";
   };
+  var span4 = document.getElementById("guy-close");
+  span3.onclick = function () {
+    guyLogInModal.style.diplay = "none";
+  };
   window.onclick = function (event) {
     if (event.target == loginModal) {
       loginModal.style.display = "none";
@@ -96,6 +110,8 @@ ready(function () {
       signUpModal.style.display = "none";
     } else if (event.target == restLogInModal) {
       restLogInModal.style.display = "none";
+    } else if (event.target == guyLogInModal) {
+      guyLogInModal.style.display = "none";
     }
   };
 });
---
+++ js/guyLogin.js
@@ -0,0 +1,30 @@
+var loginForm = document.getElementsByClassName("guy-login-form")[0];
+// var loginFirm = document.getElementById("login-form")[0];
+
+loginForm.onsubmit = (e) => {
+  e.preventDefault();
+  var username = document.getElementById("guy_username").value;
+  var password = document.getElementById("guy_password").value;
+  //   e.preventDefault();
+  // ../../api/user/login
+  var body = JSON.stringify({
+    username: username,
+    password: password,
+  });
+  // console.log(rest_id, password);
+  // console.log(body);
+  fetch("https://knight-foodji.herokuapp.com/api/deliveryguy/login", {
+    method: "POST",
+    mode: "cors",
+    headers: { "Content-Type": "application/json" },
+    body: body,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      localStorage.setItem("foodji-guy-auth-header", "Bearer " + data.token);
+      localStorage.setItem("foodji-guy", JSON.stringify(data.restaurant));
+      location.reload();
+    })
+    .catch((err) => console.log(err));
+  //   //   console.log(phone, password);
+};
