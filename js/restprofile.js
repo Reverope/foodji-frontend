fb888a78585d789cf938462dda2086f2f35a9725
knight-18 <sameershrivastava46@gmail.com>
2020-06-16 16:15:45 +0530

    updated restprofile

--- js/restprofile.js
+++ js/restprofile.js
@@ -13,7 +13,7 @@ var container = document.querySelector("#ritem");
 var containerBig = document.getElementById("foodlistingbox");
 var addFoodForm = document.getElementById("addFoodForm");
 var orderDisplay = document.querySelector(".tableoforder");
-var restIdDisplay = document.getElementById("dID");
+// var restIdDisplay = document.getElementById("dID");
 
 restaurantNameDisplay.innerHTML =
   '<img src="/assets/images/loading.gif" style="width:4rem">';
@@ -41,7 +41,7 @@ fetch(restProfileURL, {
     var restaurantOrders = restaurant["orders"];
     var restaurantAddress = restaurant["address"];
 
-    restIdDisplay.innerText = restaurantId;
+    // restIdDisplay.innerText = restaurantId;
     document.title = restaurantName;
     restaurantNameDisplay.innerText = restaurantName;
     restaurantNameDisplay2.innerText = restaurantName;
@@ -60,124 +60,6 @@ fetch(restProfileURL, {
       var price = food.childNodes[1].childNodes[3].childNodes[1].childNodes[3];
       price["innerText"] = foodItem.price;
       container.after(food);
-    });
-
-    [...restaurantOrders].forEach((orderId) => {
-      var tablerow = document.createElement("tr");
-
-      var liElement = document.createElement("td");
-      var liAddress = document.createElement("td");
-      var liPaymentStatus = document.createElement("td");
-      var liTotalPrice = document.createElement("td");
-      var liAssignmentDate = document.createElement("td");
-      var liContact = document.createElement("td");
-      var liETA = document.createElement("td");
-      var accept = document.createElement("td");
-      var decline = document.createElement("td");
-
-      tablerow.appendChild(liElement);
-      tablerow.appendChild(liAddress);
-      tablerow.appendChild(liTotalPrice);
-      tablerow.appendChild(liAssignmentDate);
-      tablerow.appendChild(liContact);
-      tablerow.appendChild(liETA);
-      tablerow.appendChild(accept);
-      tablerow.appendChild(decline);
-
-      var acceptButton = document.createElement("button");
-      var declineButton = document.createElement("button");
-      declineButton.id = orderId;
-      acceptButton.id = orderId;
-
-      accept.appendChild(acceptButton);
-      decline.appendChild(declineButton);
-
-      acceptButton.style.margin = "0 1rem";
-      declineButton.style.margin = "0 1rem";
-      acceptButton.className = "acceptdecision template-btn template-btn2";
-      declineButton.className = "declinedecision template-btn template-btn2";
-      acceptButton.innerText = "Accept";
-      declineButton.innerText = "Decline";
-
-      var url = `https://knight-foodji.herokuapp.com/api/restaurant/order/${orderId}`;
-
-      fetch(url, {
-        accept: "application/json",
-        mode: "cors",
-        method: "GET",
-        headers: {
-          Authorization: token,
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-  
-          liElement.innerText = orderId;
-          liAddress.innerText = data["address"];
-          liTotalPrice.innerText = data["payment"]["total"];
-          liContact.innerText = data["user"]["phone"];
-
-          var time = data["updatedAt"];
-          var timing = Date(time);
-
-          liAssignmentDate.innerText = timing.substr(0, 24);
-
-          // Triggering event : Accept/Decline
-
-          orderDisplay.appendChild(tablerow);
-
-          var selectAllAcceptButtons = document.querySelectorAll(
-            ".acceptdecision"
-          );
-          var selectAllDeclineButtons = document.querySelectorAll(
-            ".declinedecision"
-          );
-
-          selectAllAcceptButtons.forEach((button) => {
-            button.addEventListener("click", (clickedButton) => {
-        
-              var url =
-                "https://knight-foodji.herokuapp.com/api/restaurant/order/acceptreject/accept/" +
-                clickedButton.target.id;
-              fetch(url, {
-                accept: "application/json",
-                mode: "cors",
-                method: "POST",
-                headers: {
-                  Authorization: token,
-                },
-              }).then((response) => {
-                if (response.status == 200) {
-                  console.log("Accepted");
-                } else {
-                  console.log("Error");
-                }
-              });
-            });
-          });
-          selectAllDeclineButtons.forEach((button) => {
-            button.addEventListener("click", (clickedButton) => {
-              console.log(clickedButton.target.id);
-              var url =
-                "https://knight-foodji.herokuapp.com/api/restaurant/order/acceptreject/reject/" +
-                clickedButton.target.id;
-              fetch(url, {
-                accept: "application/json",
-                mode: "cors",
-                method: "POST",
-                headers: {
-                  Authorization: token,
-                },
-              }).then((response) => {
-                if (response.status == 200) {
-                  console.log("Rejected");
-                } else {
-                  console.log("Error");
-                }
-              });
-            });
-          });
-        });
     });
   })
   .then((_) => {
