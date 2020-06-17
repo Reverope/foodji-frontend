f5c31eb044493dd642f28032426495d6ba6c94fe
knight-18 <sameershrivastava46@gmail.com>
2020-06-17 16:02:50 +0530

    updated assign delivery guy

---
+++ js/delguyprofile.js
@@ -0,0 +1,105 @@
+var loader = document.querySelector(".preloader");
+// var profileImg = document.getElementsByClassName("author_img");
+var profileName = document.getElementById("name");
+var profileNumber = document.getElementById("about");
+var orderDisplay = document.querySelector(".tableoforder");
+var container = document.querySelector("#ritem");
+
+var url = "https://knight-foodji.herokuapp.com/api/deliveryguy/me";
+
+var token = localStorage.getItem("foodji-guy-auth-header");
+
+fetch(url, {
+  accept: "application/json",
+  mode: "cors",
+  method: "GET",
+
+  headers: {
+    Authorization: token,
+  },
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+
+    profileName.innerHTML = data.name;
+    profileNumber.innerHTML = data.email;
+    data.orders.forEach((ord) => {
+      var orderId = ord._id
+      var tablerow = document.createElement("tr");
+      var liElement = document.createElement("td");
+      var liAddress = document.createElement("td");
+      var liPaymentStatus = document.createElement("td");
+      var liTotalPrice = document.createElement("td");
+      var liAssignmentDate = document.createElement("td");
+      var liContact = document.createElement("td");
+      var liETA = document.createElement("td");
+      var accept = document.createElement("td");
+      var decline = document.createElement("td");
+      var liStatus = document.createElement("td");
+
+      tablerow.appendChild(liElement);
+      tablerow.appendChild(liAddress);
+      tablerow.appendChild(liTotalPrice);
+      tablerow.appendChild(liAssignmentDate);
+      tablerow.appendChild(liContact);
+      tablerow.appendChild(liETA);
+      tablerow.appendChild(liStatus);
+      tablerow.appendChild(accept);
+      tablerow.appendChild(decline);
+
+      var orderurl = `https://knight-foodji.herokuapp.com/api/deliveryguy/order/${orderId}`;
+
+      fetch(orderurl, {
+        accept: "application/json",
+        mode: "cors",
+        method: "GET",
+        headers: {
+          Authorization: token,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+        
+          var orderedFoodList = data["foods"];
+          console.log(orderedFoodList);
+
+          [...orderedFoodList].forEach((food) => {
+            liElement.innerHTML +=
+              "<li><p>" +
+              food["name"] +
+              "(x" +
+              food["quantity"] +
+              ")  <br> ₹" +
+              food["price"] +
+              "</p>" +
+              "</li>";
+          });
+
+          // liElement.innerText = orderId;
+          liAddress.innerText = data["address"];
+          liTotalPrice.innerText = data["payment"]["total"];
+          liContact.innerText = data["user"]["phone"];
+          liStatus.innerText = data["status"];
+          liETA.innerText = `30 minutes`
+
+          var time = data["updatedAt"];
+          var timing = Date(time);
+
+          liAssignmentDate.innerText = timing.substr(0, 24);
+
+          // Triggering event : Accept/Decline
+
+          orderDisplay.appendChild(tablerow);
+        });
+    });
+
+  })
+  .then((_) => {
+    loader.remove();
+  })
+  .catch((err) => {
+    console.log(err)
+    PopUpLog();
+  })
